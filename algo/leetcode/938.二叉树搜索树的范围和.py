1aacca7732d20345a719083f4671e6143a98345b
LR2933 <r2933985616@gmail.com>
2026-06-03 11:15:57 +0800

    algo: 938.二叉树搜索树的范围和.py

---
+++ algo/leetcode/938.二叉树搜索树的范围和.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        self.ans = 0
+
+        def dfs(node):
+            if node is None:
+                return
+
+            if node.val > low:
+                dfs(node.left)
+
+            if low <= node.val <= high:
+                self.ans += node.val
+
+            if node.val < high:
+                dfs(node.right)
+
+        dfs(root)
+        return self.ans
