ee3244fa354440c044ea00b7d45d715a2a8ac04f
LR2933 <r2933985616@gmail.com>
2026-06-04 20:05:57 +0800

    CS61A:hw08: finish Q1

--- CS61A/hw08/hw08.scm
+++ CS61A/hw08/hw08.scm
@@ -1,4 +1,11 @@
-(define (ascending? s) 'YOUR-CODE-HERE)
+(define (ascending? s)
+  (cond
+    ((null? s) #t)
+    ((null? (cdr s)) #t)
+    ((<= (car s) (car(cdr s))) (ascending? (cdr s)))
+    (else #f)
+  )
+)
 
 (define (my-filter pred s) 'YOUR-CODE-HERE)
 
