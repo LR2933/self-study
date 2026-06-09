81db2f96c55256cebfa6e3c11662ad71f294af80
LR2933 <r2933985616@gmail.com>
2026-06-09 19:42:11 +0800

    algo: 103-二叉树的锯齿形层序遍历.py

---
+++ algo/leetcode/103-二叉树的锯齿形层序遍历.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+
+        queue = deque([root])
+        reverse = False
+        ret = []
+
+        while queue:
+            current_level = []
+
+            for _ in range(len(queue)):
+                node = queue.popleft()
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+                current_level.append(node.val)
+
+            if reverse:
+                current_level.reverse()
+
+            ret.append(current_level)
+            reverse = not reverse
+
+        return ret
+
