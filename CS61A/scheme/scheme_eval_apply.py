95168e25244ac6a24f545153a266961000bf1560
LR2933 <r2933985616@gmail.com>
2026-06-14 10:25:10 +0800

    CS61A:scheme: finish q2

--- CS61A/scheme/scheme_eval_apply.py
+++ CS61A/scheme/scheme_eval_apply.py
@@ -46,11 +46,16 @@ def scheme_apply(procedure, args, env):
        assert False, "Not a Frame: {}".format(env)
     if isinstance(procedure, BuiltinProcedure):
         # BEGIN PROBLEM 2
-        "*** YOUR CODE HERE ***"
+        args_list = []
+        while args:
+            args_list.append(args.first)
+            args = args.rest
+        if procedure.need_env:
+            args_list.append(env)
         # END PROBLEM 2
         try:
             # BEGIN PROBLEM 2
-            "*** YOUR CODE HERE ***"
+            return procedure.py_func(*args_list)
             # END PROBLEM 2
         except TypeError as err:
             raise SchemeError('incorrect number of arguments: {0}'.format(procedure))
