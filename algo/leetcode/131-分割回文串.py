a59101ca4a4838b9e43f899da3e4b190e812e9c6
LR2933 <r2933985616@gmail.com>
2026-06-18 17:11:42 +0800

    algo: 131-分割回文串.py

---
+++ algo/leetcode/131-分割回文串.py
@@ -0,0 +1,22 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        n = len(s)
+        ret = []
+        path = []
+
+        def dfs(i:int, start:int):
+            if i == n:
+                ret.append(path[:])
+                return
+
+            if i < n - 1:
+                dfs(i + 1, start)
+
+            t = s[start:i + 1]
+            if t == t[::-1]:
+                path.append(t)
+                dfs(i + 1, i + 1)
+                path.pop()
+
+        dfs(0, 0)
+        return ret
