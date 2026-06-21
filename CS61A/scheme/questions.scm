4649f5237212cef66edd191bd25ad7a5d8df3b8a
LR2933 <r2933985616@gmail.com>
2026-06-21 20:15:38 +0800

    CS61A:scheme: finish q15

--- CS61A/scheme/questions.scm
+++ CS61A/scheme/questions.scm
@@ -22,22 +22,28 @@
 ;; Return the value for a key in a dictionary list
 (define (get dict key)
   ; BEGIN PROBLEM 15
-  'replace-this-line
+  (cond
+    ((null? dict) #f)
+    ((equal? (car (car dict)) key) (car (cdr (car dict))))
+    (else (get (cdr dict) key))))
   ; END PROBLEM 15
-  )
+  
 
 ;; Return a dictionary list with a (key value) pair
 (define (set dict key val)
+    (cond
+      ((null? dict) (cons (list key val) '()))
+      ((equal? (car (car dict)) key) (cons (list key val) (cdr dict)))
+      (else (cons (list (car (car dict)) (car (cdr (car dict)))) (set (cdr dict) key val)))))
   ; BEGIN PROBLEM 15
-  'replace-this-line
   ; END PROBLEM 15
-  )
+  
 
 ;; Problem 16
 
 ;; implement solution-code
 (define (solution-code problem solution)
   ; BEGIN PROBLEM 16
-  'replace-this-line
+  'replace-this-line)
   ; END PROBLEM 16
-  )
+  
