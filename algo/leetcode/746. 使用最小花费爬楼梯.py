582d9c35acf74e6630832d8d8bfba32e0005fa18
LR2933 <r2933985616@gmail.com>
2026-06-26 15:07:48 +0800

    algo: 746. 使用最小花费爬楼梯.py(迭代解法)

--- algo/leetcode/746. 使用最小花费爬楼梯.py
+++ algo/leetcode/746. 使用最小花费爬楼梯.py
@@ -1,19 +1,10 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         n = len(cost)
-        cost.append(0)
-        memo = {}
+        dp = [0, 0]
 
-        def dfs(i):
-            if i == 0:
-                return cost[0]
-            elif i == 1:
-                return cost[1]
+        for i in range(2, n + 1):
+            curr = min(dp[0] + cost[i - 2], dp[1] + cost[i - 1])
+            dp[0], dp[1] = dp[1], curr
 
-            if i in memo:
-                return memo[i]
-            else:
-                memo[i] = cost[i] + min(dfs(i - 1), dfs(i - 2))
-                return memo[i]
-
-        return dfs(n)
+        return dp[-1]
