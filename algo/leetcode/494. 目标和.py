480dc24fcd023b71a18ac9d831df6a744be80f45
LR2933 <r2933985616@gmail.com>
2026-07-31 13:08:44 +0800

    algo: 494. 目标和.py
    动态规划解法

--- algo/leetcode/494. 目标和.py
+++ algo/leetcode/494. 目标和.py
@@ -1,12 +1,18 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        s = sum(nums)
         n = len(nums)
+        of = s
+        f = [[0] * (s * 2 + 1) for _ in range(n)]
+        f[0][nums[0] + of] += 1
+        f[0][-nums[0] + of] += 1
 
-        @cache
-        def dfs(i, t):
-            if i == n:
-                return 1 if t == 0 else 0
+        j_max = s * 2 + 1
+        for i in range(n - 1):
+            for j in range(j_max):
+                if j - nums[i + 1] >= 0:
+                    f[i + 1][j - nums[i + 1]] += f[i][j]
+                if j + nums[i + 1] < j_max:
+                    f[i + 1][j + nums[i + 1]] += f[i][j]
 
-            return dfs(i + 1, t + nums[i]) + dfs(i + 1, t - nums[i])
-
-        return 0 if sum(nums) < abs(target) else dfs(0, target)
+        return 0 if s < abs(target) else f[-1][target + of]
